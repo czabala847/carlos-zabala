eef131e277982ab7aba163f038366108079c2da9
Carlos Zabala <czabala847@gmail.com>
2021-11-06 11:36:47 -0500

    Realizando hook para el render en el canvas}

--- src/components/HeroCanvas.js
+++ src/components/HeroCanvas.js
@@ -3,55 +3,36 @@ import React from "react";
 import "../styles/components/HeroCanvas.scss";
 
 import imgSurface from "../assets/img/hero_footer.png";
+import { useDrawCanvas } from "../hooks/useDrawCanvas";
 
 function HeroCanvas() {
   const canvasRef = React.useRef(null);
-
-  const drawImage = (ctx, urlImage) => {
-    const widthCanvas = ctx.canvas.width;
-    const heightCanvas = ctx.canvas.height;
-    //Imagen de la superficie
-    const img = new Image();
-    img.src = urlImage;
-
-    img.onload = () => {
-      ctx.save();
-      ctx.globalAlpha = 0.1;
-
-      const heightImage = heightCanvas * 0.4;
-      const yImage = heightCanvas * 0.6;
-      const widthImage = widthCanvas;
-
-      ctx.drawImage(img, 0, yImage, widthImage, heightImage);
-      ctx.restore();
-    };
-  };
+  const { drawImage, drawStars } = useDrawCanvas();
 
   React.useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
 
-    const displayWidth = canvas.clientWidth;
-    const displayHeight = canvas.clientHeight;
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
 
-    canvas.width = displayWidth;
-    canvas.height = displayHeight;
-
-    let frameCount = 0;
-    let animationFrameId;
+    // let frameCount = 0;
+    // let animationFrameId;
 
     const render = () => {
-      frameCount++;
-      drawStart(ctx, frameCount);
-      drawImage(ctx, imgSurface);
+      // frameCount++;
+      // drawStart(ctx, frameCount);
+      // drawImage(ctx, imgSurface);
       // animationFrameId = window.requestAnimationFrame(render);
+      drawStars(ctx);
+      drawImage(ctx, imgSurface);
     };
 
     render();
 
-    return () => {
-      window.cancelAnimationFrame(animationFrameId);
-    };
+    // return () => {
+    //   window.cancelAnimationFrame(animationFrameId);
+    // };
   }, []);
 
   return (
---
+++ src/hooks/useDrawCanvas.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+function useDrawCanvas() {
+  const [stars, setStars] = React.useState([]);
+
+  const randomNumber = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+
+  const generateStars = (quantity, limit_x, limit_y) => {
+    const arrayStars = [];
+
+    for (let i = 0; i < quantity; i++) {
+      const x = randomNumber(0, limit_x);
+      const y = randomNumber(0, limit_y);
+
+      const size = randomNumber(1, 3);
+
+      arrayStars.push({ x, y, size });
+    }
+
+    setStars(arrayStars);
+  };
+
+  const drawStars = (ctx) => {
+    const height = ctx.canvas.height;
+    const width = ctx.canvas.width;
+
+    ctx.clearRect(0, 0, width, height);
+
+    ctx.save();
+
+    //Estilos
+    ctx.fillStyle = "white";
+    ctx.shadowColor = "white";
+    ctx.strokeStyle = "rgba(0,0,0,0.1)";
+    ctx.shadowBlur = 5;
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 1;
+    //
+
+    generateStars(150, width, height * 0.7);
+
+    stars.forEach((star) => {
+      ctx.beginPath();
+      ctx.arc(star.x, star.y, star.size, 0, 2 * Math.PI);
+      ctx.fill();
+    });
+
+    ctx.restore();
+  };
+
+  const drawImage = (ctx, urlImage) => {
+    const height = ctx.canvas.height;
+    const width = ctx.canvas.width;
+
+    const img = new Image();
+    img.src = urlImage;
+
+    img.onload = () => {
+      ctx.save();
+      ctx.globalAlpha = 0.1;
+
+      const heightImage = height * 0.4;
+      const yImage = height * 0.6;
+      const widthImage = width;
+
+      ctx.drawImage(img, 0, yImage, widthImage, heightImage);
+      ctx.restore();
+    };
+  };
+
+  return { drawImage, drawStars };
+}
+
+export { useDrawCanvas };
