466bf1781a4c16e81131edf1c156f84235b3c231
Carlos Zabala <czabala847@gmail.com>
2022-07-07 08:47:31 -0500

    Añadiendo metatags en el home principal

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,20 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name="keywords"
+          content="Carlos Zabala, Desarrollo web, Front-end, Back-end, frontend, backend"
+        />
+        <meta name="author" content="Carlos Zabala Roqueme" />
+        <meta
+          name="copyright"
+          content="Carlos Zabala, todos los derechos reservados."
+        />
+        <meta name="robots" content="ALL" />
+        <link
+          rel="canonical"
+          href="https://czabala847.github.io/carlos-zabala/#/"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;700&display=swap"
           rel="stylesheet"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 
 import { Hero } from "containers/Hero";
 import { Projects } from "containers/Projects";
@@ -6,10 +7,19 @@ import { HomeContainer } from "styles/pages/HomeStyles";
 
 const Home = () => {
   return (
-    <HomeContainer>
-      <Hero />
-      <Projects />
-    </HomeContainer>
+    <>
+      <Head>
+        <title>Carlos Zabala</title>
+        <meta
+          name="description"
+          content="Hola, soy Carlos Zabala Desarrollador Web, apasionado con el desarrollo front-end y con experiencia en lenguajes back-end."
+        />
+      </Head>
+      <HomeContainer>
+        <Hero />
+        <Projects />
+      </HomeContainer>
+    </>
   );
 };
 
