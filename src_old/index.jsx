d7698d47a36a2dbedcd0536627ad0d06a2a072eb
Carlos Zabala <czabala847@gmail.com>
2022-07-07 15:15:42 -0500

    Eliminando src_old

--- src_old/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./styles/Global.scss";
-
-import { App } from "./routes/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
